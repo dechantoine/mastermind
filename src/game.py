0dc938f3547147f4f48b851b8f4584eac8986ba8
Antoine Déchappe <dechantoine@gmail.com>
2024-09-27 11:26:25 +0200

    feat: game with colors emojis

--- src/game.py
+++ src/game.py
@@ -1,5 +1,93 @@
+import json
+from typing import Union
+import os
+
 from loguru import logger
 import numpy as np
+
+emojis = json.load(open(os.path.dirname(__file__) + "/emojis.json", "r"))
+
+
+def get_emojis(n_colors: int = 8) -> str:
+    """Get the emojis.
+
+    Args:
+        n_colors (int): number of colors
+
+    Returns:
+        str: the emojis for playing with this number of colors
+    """
+    return "".join(emoji for emoji in list(emojis.values())[:n_colors])
+
+
+def code_to_emoji(code: list[int]) -> str:
+    """Convert a code to emojis.
+
+    Args:
+        code (list[int]): the code
+
+    Returns:
+        str: the emojis representing the code
+    """
+    return "".join(emojis[str(color)] for color in code)
+
+
+def emojis_to_code(emoji_str: str) -> list[int]:
+    """Convert emojis to a code.
+
+    Args:
+        emoji_str (str): the emojis representing the code
+
+    Returns:
+        list: the code
+    """
+    return [list(emojis.values()).index(emoji) for emoji in list(emoji_str)]
+
+
+def generate_code(n_colors: int = 8, len_code: int = 5) -> list[int]:
+    """Generate a random code by sampling len_code from n_colors.
+
+    Args:
+        n_colors (int): number of colors
+        len_code (int): length of the code
+
+    Returns:
+        list: the code
+    """
+    return np.random.randint(low=0,
+                             high=n_colors,
+                             size=len_code).tolist()
+
+
+def check_code(guess: list[int], code: list[int]) -> tuple[int, int]:
+    """
+    Check the guess against the code.
+
+    Args:
+        guess (list[int]): the guess
+        code (list[int]): the code
+
+    Returns:
+        tuple: number of right positions, number of wrong positions
+    """
+    if len(guess) != len(code):
+        raise ValueError(f"Guess and code must be of the same length")
+
+    remaining_code = [c for i, c in enumerate(code)
+                      if guess[i] != c]
+    remaining_guess = [c for i, c in enumerate(guess)
+                       if code[i] != c]
+
+    right_positions = len(code) - len(remaining_code)
+    wrong_positions = 0
+
+    while remaining_guess:
+        c = remaining_guess.pop()
+        if c in remaining_code:
+            remaining_code.remove(c)
+            wrong_positions += 1
+
+    return right_positions, wrong_positions
 
 
 class MasterMind:
@@ -7,7 +95,8 @@ class MasterMind:
                  n_colors: int = 8,
                  len_code: int = 5,
                  n_rounds: int = 12,
-                 code: list[int] = None) -> None:
+                 code: Union[list[int], str] = None,
+                 use_emojis: bool = False) -> None:
         """
         Initialize the game of MasterMind.
 
@@ -15,61 +104,32 @@ class MasterMind:
             n_colors (int): number of colors in the game
             len_code (int): length of the code
             n_rounds (int): number of rounds
-            code (list[int]): code to guess (if None, a random code is generated)
+            code (Union[list[int], str]): code to guess (if None, a random code is generated)
+            use_emojis (bool): whether to use emojis for colors
         """
         self.n_colors = n_colors
         self.len_code = len_code
         self.n_rounds = n_rounds
+        self.use_emojis = use_emojis
 
         self.game_over = False
+        self.win = None
 
         self.rounds = {}
 
         if code is not None:
+            if isinstance(code, str):
+                code = emojis_to_code(code)
+
             if len(code) != len_code:
                 raise ValueError(f"Code must be of length {len_code}")
             if not all(0 <= color < n_colors for color in code):
-                raise ValueError(f"Colors must be in the range 0 to {n_colors -1}")
+                raise ValueError(f"Colors must be in the range 0 to {n_colors - 1}")
+
             self.code = code
 
         else:
-            self._generate_code()
-
-    def _generate_code(self) -> None:
-        """Generate a random code for the game."""
-        self.code = np.random.randint(0, self.n_colors, self.len_code)
-
-    def check_code(self, guess: list[int]) -> tuple[int, int]:
-        """
-        Check the guess against the code.
-
-        Args:
-            guess (list[int]): the guess
-
-        Returns:
-            tuple: number of right positions, number of wrong positions
-        """
-        if len(guess) != self.len_code:
-            raise ValueError(f"Guess must be of length {self.len_code}")
-
-        if not all(0 <= color < self.n_colors for color in guess):
-            raise ValueError(f"Colors must be in the range 0 to {self.n_colors -1}")
-
-        remaining_code = [c for i, c in enumerate(self.code)
-                          if guess[i] != c]
-        remaining_guess = [c for i, c in enumerate(guess)
-                           if self.code[i] != c]
-
-        right_positions = self.len_code - len(remaining_code)
-        wrong_positions = 0
-
-        while remaining_guess:
-            c = remaining_guess.pop()
-            if c in remaining_code:
-                remaining_code.remove(c)
-                wrong_positions += 1
-
-        return right_positions, wrong_positions
+            self.code = generate_code(n_colors=n_colors, len_code=len_code)
 
     def _play_turn(self, guess: list[int]):
         """
@@ -77,11 +137,12 @@ class MasterMind:
 
         Args:
             guess (list[int]): the guess
-
-        Return:
-            dict: the results of the turns so far
         """
-        right_positions, wrong_positions = self.check_code(guess)
+        if not all(0 <= color < self.n_colors for color in guess):
+            raise ValueError(f"Colors must be in the range 0 to {self.n_colors - 1}")
+
+        right_positions, wrong_positions = check_code(guess=guess, code=self.code)
+
         self.rounds[len(self.rounds)] = {"guess": guess,
                                          "right_positions": right_positions,
                                          "wrong_positions": wrong_positions}
@@ -89,13 +150,29 @@ class MasterMind:
         if right_positions == self.len_code:
             logger.info("Code guessed!")
             self.game_over = True
-
-        return self.rounds
-
-    def __call__(self, guess: list[int]):
-        if len(self.rounds) >= self.n_rounds or self.game_over:
-            logger.warning("Game over.")
-            self.game_over = True
+            self.win = True
             return self.rounds
 
-        return self._play_turn(guess)
+        if len(self.rounds) == self.n_rounds:
+            logger.info("Game over: out of rounds.")
+            self.game_over = True
+            self.win = False
+
+    def __call__(self, guess: Union[list[int], str]) -> dict:
+        if self.game_over:
+            logger.warning("Game over.")
+            return self.rounds
+
+        if isinstance(guess, str):
+            guess = emojis_to_code(guess)
+
+        self._play_turn(guess)
+        rounds = {k: v.copy() for k, v in self.rounds.items()}
+
+        if not self.use_emojis:
+            return rounds
+
+        else:
+            for _, round_ in rounds.items():
+                round_["guess"] = code_to_emoji(round_["guess"])
+            return rounds
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,5 @@
 import unittest
-from src.game import MasterMind
+from src.game import MasterMind, generate_code, check_code
 
 
 class TestMasterMind(unittest.TestCase):
@@ -28,47 +28,41 @@ class TestMasterMind(unittest.TestCase):
 
     def test_generate_code(self):
         game = MasterMind(n_colors=6, len_code=4, n_rounds=10)
-        game._generate_code()
+        generate_code()
         self.assertEqual(len(game.code), 4)
         self.assertTrue(all(0 <= color < 6 for color in game.code))
 
     def test_check_code(self):
-        game = MasterMind(n_colors=6, len_code=4, n_rounds=10)
-        game.code = [1, 2, 3, 4]
+        code = [1, 2, 3, 4]
 
         # Test correct guess
         guess = [1, 2, 3, 4]
-        right_positions, wrong_positions = game.check_code(guess)
+        right_positions, wrong_positions = check_code(guess, code)
         self.assertEqual(right_positions, 4)
         self.assertEqual(wrong_positions, 0)
 
         # Test completely wrong guess
         guess = [4, 3, 2, 1]
-        right_positions, wrong_positions = game.check_code(guess)
+        right_positions, wrong_positions = check_code(guess, code)
         self.assertEqual(right_positions, 0)
         self.assertEqual(wrong_positions, 4)
 
         # Test partially correct guess
         guess = [1, 2, 4, 3]
-        right_positions, wrong_positions = game.check_code(guess)
+        right_positions, wrong_positions = check_code(guess, code)
         self.assertEqual(right_positions, 2)
         self.assertEqual(wrong_positions, 2)
 
         # Test guess with no correct positions
         guess = [5, 5, 5, 5]
-        right_positions, wrong_positions = game.check_code(guess)
+        right_positions, wrong_positions = check_code(guess, code)
         self.assertEqual(right_positions, 0)
         self.assertEqual(wrong_positions, 0)
 
         # Test raise error for wrong length guess
         guess = [1, 2, 3]
         with self.assertRaises(ValueError):
-            game.check_code(guess)
-
-        # Test raise error for out of range colors
-        guess = [1, 2, 3, 6]
-        with self.assertRaises(ValueError):
-            game.check_code(guess)
+            check_code(guess, code)
 
     def test_play_turn(self):
         game = MasterMind(n_colors=6, len_code=4, n_rounds=10)
@@ -76,31 +70,31 @@ class TestMasterMind(unittest.TestCase):
 
         # Test correct guess
         guess = [1, 2, 3, 4]
-        rounds = game._play_turn(guess)
-        self.assertEqual(rounds[0]['guess'], guess)
-        self.assertEqual(rounds[0]['right_positions'], 4)
-        self.assertEqual(rounds[0]['wrong_positions'], 0)
+        game._play_turn(guess)
+        self.assertEqual(game.rounds[0]['guess'], guess)
+        self.assertEqual(game.rounds[0]['right_positions'], 4)
+        self.assertEqual(game.rounds[0]['wrong_positions'], 0)
 
         # Test completely wrong guess
         guess = [4, 3, 2, 1]
-        rounds = game._play_turn(guess)
-        self.assertEqual(rounds[1]['guess'], guess)
-        self.assertEqual(rounds[1]['right_positions'], 0)
-        self.assertEqual(rounds[1]['wrong_positions'], 4)
+        game._play_turn(guess)
+        self.assertEqual(game.rounds[1]['guess'], guess)
+        self.assertEqual(game.rounds[1]['right_positions'], 0)
+        self.assertEqual(game.rounds[1]['wrong_positions'], 4)
 
         # Test partially correct guess
         guess = [1, 2, 4, 3]
-        rounds = game._play_turn(guess)
-        self.assertEqual(rounds[2]['guess'], guess)
-        self.assertEqual(rounds[2]['right_positions'], 2)
-        self.assertEqual(rounds[2]['wrong_positions'], 2)
+        game._play_turn(guess)
+        self.assertEqual(game.rounds[2]['guess'], guess)
+        self.assertEqual(game.rounds[2]['right_positions'], 2)
+        self.assertEqual(game.rounds[2]['wrong_positions'], 2)
 
         # Test guess with no correct positions
         guess = [5, 5, 5, 5]
-        rounds = game._play_turn(guess)
-        self.assertEqual(rounds[3]['guess'], guess)
-        self.assertEqual(rounds[3]['right_positions'], 0)
-        self.assertEqual(rounds[3]['wrong_positions'], 0)
+        game._play_turn(guess)
+        self.assertEqual(game.rounds[3]['guess'], guess)
+        self.assertEqual(game.rounds[3]['right_positions'], 0)
+        self.assertEqual(game.rounds[3]['wrong_positions'], 0)
 
         # Test raise error for wrong length guess
         guess = [1, 2, 3]
@@ -112,17 +106,17 @@ class TestMasterMind(unittest.TestCase):
         with self.assertRaises(ValueError):
             game._play_turn(guess)
 
-        self.assertEqual(len(rounds), 4)
+        self.assertEqual(len(game.rounds), 4)
 
     def test_call(self):
         game = MasterMind(n_colors=6, len_code=4, n_rounds=3)
         game.code = [1, 2, 3, 4]
 
         # Test correct guess
-        guess = [1, 2, 3, 4]
+        guess = [1, 2, 3, 5]
         rounds = game(guess)
         self.assertEqual(rounds[0]['guess'], guess)
-        self.assertEqual(rounds[0]['right_positions'], 4)
+        self.assertEqual(rounds[0]['right_positions'], 3)
         self.assertEqual(rounds[0]['wrong_positions'], 0)
 
         # Test completely wrong guess
@@ -144,3 +138,9 @@ class TestMasterMind(unittest.TestCase):
         rounds = game(guess)
         self.assertEqual(len(rounds), 3)
         self.assertNotIn(3, rounds)
+
+        # Test game over
+        guess = [1, 2, 3, 4]
+        rounds = game(guess)
+        self.assertEqual(len(rounds), 3)
+        self.assertNotIn(4, rounds)
